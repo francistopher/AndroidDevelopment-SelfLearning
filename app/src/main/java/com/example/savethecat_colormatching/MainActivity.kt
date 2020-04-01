e7c2c42bf9ad87c437b53139b5baa2a15d73d090
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-04-01 10:22:21 -0400

    Update main view to hide action bar

--- app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
+++ app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.savethecat_colormatching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        var staticSelf: MainActivity? = null
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        staticSelf = this
+
+
     }
 }
