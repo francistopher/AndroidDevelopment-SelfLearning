ced82b435a53dd6502e0ff092872495d64cbffb4
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-04-01 12:52:06 -0400

    Create ability to get current theme on Android 10

--- app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
+++ app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
@@ -1,37 +1,26 @@
 package com.example.savethecat_colormatching
 
 import android.content.res.Configuration
-import android.content.res.Resources
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.service.autofill.Validators.and
 import android.util.Log
 import android.view.View
-import android.view.Window
-import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         var staticSelf: MainActivity? = null
         var rootView: View? = null
-        var isThemeLight:Boolean = true
+        var isThemeDark:Boolean = true
     }
 
-
-
-
     fun setCurrentTheme() {
-        // Set theme for android pie
         when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
-            Configuration.UI_MODE_NIGHT_YES -> isThemeLight = false
+            Configuration.UI_MODE_NIGHT_NO -> isThemeDark = false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> isThemeDark = false
         }
-        Log.i("Color?", "${isThemeLight}")
+        Log.i("Color?", "${isThemeDark}")
     }
 
 
