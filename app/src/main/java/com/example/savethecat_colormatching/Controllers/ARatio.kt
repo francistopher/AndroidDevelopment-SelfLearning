3d76eea89528820907f28af93d30761342a2b3ff
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-04-01 15:24:19 -0400

    Create function to display screen dimensions

---
+++ app/src/main/java/com/example/savethecat_colormatching/Controllers/ARatio.kt
@@ -0,0 +1,6 @@
+enum class ARatio {
+    ar4by3, ar3by2, ar16by10, ar5by3,
+    ar16by9, ar19by10, ar18by9,
+    ar18point5by9, ar18point7by9,
+    ar19by9, ar19point5by9
+}
--- app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
+++ app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.savethecat_colormatching
 
 
+import ARatio
 import Reachability
 import android.content.IntentFilter
 import android.content.res.Configuration
 import android.graphics.Color
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,9 +21,8 @@ class MainActivity : AppCompatActivity(), Reachability.ConnectivityReceiverListe
         var rootView: View? = null
         var isThemeDark:Boolean = true
         var isInternetReachable:Boolean = false
+        var aspectRatio:ARatio? = null
     }
-
-
 
     private fun setCurrentTheme() {
         when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
@@ -61,9 +63,17 @@ class MainActivity : AppCompatActivity(), Reachability.ConnectivityReceiverListe
         staticSelf = this
         setCurrentTheme()
         setupReachability()
+        setupAspectRatio()
     }
 
     private fun setupReachability() {
         registerReceiver(Reachability(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    private val displayMetrics = DisplayMetrics()
+    private fun setupAspectRatio() {
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        Log.i("Screen Width", "${displayMetrics.widthPixels}")
+        Log.i("Screen Height", "${displayMetrics.heightPixels}")
     }
 }
