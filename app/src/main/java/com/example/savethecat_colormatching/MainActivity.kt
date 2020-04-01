5b37d4a2c701c64285840dcb2fb8c0d2613c2b96
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-04-01 15:54:44 -0400

    Create function to save aspect ratio and remove (navigation + notification bar)

--- app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
+++ app/src/main/java/com/example/savethecat_colormatching/MainActivity.kt
@@ -7,10 +7,12 @@ import android.content.IntentFilter
 import android.content.res.Configuration
 import android.graphics.Color
 import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -57,7 +59,13 @@ class MainActivity : AppCompatActivity(), Reachability.ConnectivityReceiverListe
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
         setContentView(R.layout.activity_main)
         rootView = window.decorView.rootView
         staticSelf = this
@@ -70,10 +78,61 @@ class MainActivity : AppCompatActivity(), Reachability.ConnectivityReceiverListe
         registerReceiver(Reachability(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
     }
 
-    private val displayMetrics = DisplayMetrics()
+    private fun getNavigationBarHeight(): Int {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            val metrics = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(metrics)
+            val usableHeight = metrics.heightPixels
+            windowManager.defaultDisplay.getRealMetrics(metrics)
+            val realHeight = metrics.heightPixels
+            return if (realHeight > usableHeight) realHeight - usableHeight else 0
+        }
+        return 0
+    }
+
+    private val displayMetrics:DisplayMetrics = DisplayMetrics()
+    private var screenAspectRatio:Double = 0.0;
     private fun setupAspectRatio() {
         windowManager.defaultDisplay.getMetrics(displayMetrics)
+        screenAspectRatio = (displayMetrics.heightPixels.toDouble() + getNavigationBarHeight().toDouble()) / (displayMetrics.widthPixels)
         Log.i("Screen Width", "${displayMetrics.widthPixels}")
         Log.i("Screen Height", "${displayMetrics.heightPixels}")
+        Log.i("Screen Aspect Ratio", "$screenAspectRatio")
+        when {
+            screenAspectRatio > 2.16 -> {
+                aspectRatio = ARatio.ar19point5by9
+            }
+            screenAspectRatio > 2.09 -> {
+                aspectRatio = ARatio.ar19by9
+            }
+            screenAspectRatio > 2.07 -> {
+                aspectRatio = ARatio.ar18point7by9
+            }
+            screenAspectRatio > 2.05 -> {
+                aspectRatio = ARatio.ar18point5by9
+            }
+            screenAspectRatio > 1.9 -> {
+                aspectRatio = ARatio.ar18by9
+            }
+            screenAspectRatio > 1.8 -> {
+                aspectRatio = ARatio.ar19by10
+            }
+            screenAspectRatio > 1.7 -> {
+                aspectRatio = ARatio.ar16by9
+            }
+            screenAspectRatio > 1.66 -> {
+                aspectRatio = ARatio.ar5by3
+            }
+            screenAspectRatio > 1.5 -> {
+                aspectRatio = ARatio.ar16by10
+            }
+            screenAspectRatio > 1.4 -> {
+                aspectRatio = ARatio.ar3by2
+            }
+            else -> {
+                // screen aspect ratio > 1.3
+                aspectRatio = ARatio.ar4by3
+            }
+        }
     }
 }
