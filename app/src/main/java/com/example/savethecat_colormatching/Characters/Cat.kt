476e9820bbb8afbbfa9977634364ff67bdfe2cb2
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-07-23 20:20:19 -0400

    CREATE Comments for Cat.kt

--- app/src/main/java/com/example/savethecat_colormatching/Characters/Cat.kt
+++ app/src/main/java/com/example/savethecat_colormatching/Characters/Cat.kt
@@ -1,5 +1,6 @@
 package com.example.savethecat_colormatching.Characters
 
+// Create enum class for all the Cats
 enum class Cat {
     STANDARD, BREADING, TACO, EGYPTIAN, SUPER, CHICKEN, COOL, NINJA, FAT
 }
