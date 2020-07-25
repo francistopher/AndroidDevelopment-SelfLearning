7ee240214394aebabed1103abecdabd8ec82d34d
Christopher Francisco <franciscochristopher77777@gmail.com>
2020-07-25 16:15:07 -0400

    UPDATE CButton.kt with more comments

--- app/src/main/java/com/example/savethecat_colormatching/CustomViews/CButton.kt
+++ app/src/main/java/com/example/savethecat_colormatching/CustomViews/CButton.kt
@@ -78,6 +78,9 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         button!!.text = text
     }
 
+    /*
+        Setup the corner radius and the border width of the button
+     */
     private var shape: GradientDrawable? = null
     private var borderWidth:Int = 0
     private var cornerRadius:Int = 0
@@ -85,6 +88,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         shape = null
         shape = GradientDrawable()
         shape!!.shape = GradientDrawable.RECTANGLE
+        // Draw the background shape
         if (backgroundColor != null) {
             shape!!.setColor(backgroundColor!!)
         } else if (targetBackgroundColor != null) {
@@ -96,6 +100,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 shape!!.setColor(Color.WHITE)
             }
         }
+        // Draw the border of the button
         if (borderWidth > 0) {
             this.borderWidth = borderWidth
             if (isBorderGold) {
@@ -108,11 +113,15 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 }
             }
         }
+        // Setup the corner radius of the button
         cornerRadius = radius
         shape!!.cornerRadius = radius.toFloat()
         button!!.setBackgroundDrawable(shape)
     }
 
+    /*
+        Scale the width of the button
+     */
     private var growWidthAndChangeColor: AnimatorSet? = null
     private var growWidthAnimator: ValueAnimator? = null
     private var transitionColorAnimator: ValueAnimator? = null
@@ -120,6 +129,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
     private var fadeOutAnimator:ValueAnimator? = null
     var growWidthAndChangeColorIsRunning: Boolean = false
     fun growWidth(width: Float) {
+        // If the width is scaling, cancel the animator
         if (growWidthAndChangeColor != null) {
             if (growWidthAndChangeColorIsRunning) {
                 growWidthAndChangeColor!!.cancel()
@@ -166,10 +176,12 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             growWidthAndChangeColor!!.play(growWidthAnimator!!).
             with(transitionColorAnimator!!)
         }
+        // Setup animator properties
         growWidthAndChangeColor!!.duration = 1000
         growWidthAndChangeColor!!.startDelay = 125
         growWidthAndChangeColorIsRunning = true
         growWidthAndChangeColor!!.start()
+        // Fade the button out after scaling width
         growWidthAndChangeColor!!.doOnEnd {
             growWidthAndChangeColorIsRunning = false
             fadeOutAnimator = ValueAnimator.ofFloat(1f, 0f)
@@ -179,7 +191,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             fadeOutAnimator!!.interpolator = EasingInterpolator(Ease.QUAD_IN_OUT)
             fadeOutAnimator!!.duration = 750
             fadeOutAnimator!!.start()
-            // Do on end
+            // Remove button from screen after fading out
             fadeOutAnimator!!.doOnEnd {
                 parentLayout!!.removeView(getThis())
             }
@@ -192,6 +204,10 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         grow = !grow
     }
 
+    /*
+        Dilates both the width and the height of the button
+        to appear to grow
+     */
     private var growAnimatorSet:AnimatorSet? = null
     private var growHeightAnimator:ValueAnimator? = null
     private var isGrowing:Boolean = false
@@ -199,9 +215,11 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
     private var height:Float = 0f
     private var y:Float = 0f
     fun grow(duration:Float, delay:Float) {
+        // Cancel shrink animation if its running
         if (shrinkAnimationSet != null) {
             shrinkAnimationSet!!.cancel()
         }
+        // Cancel grow animation if its running
         if (growAnimatorSet != null) {
             if (isGrowing) {
                 growAnimatorSet!!.cancel()
@@ -209,6 +227,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 growAnimatorSet = null
             }
         }
+        // Grow or shrink the button
         if (grow) {
             growWidthAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).width.toFloat(), originalParams!!.width.toFloat())
             growHeightAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).height.toFloat(), originalParams!!.height.toFloat())
@@ -216,6 +235,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             growWidthAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).width.toFloat(), 1f)
             growHeightAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).width.toFloat(), 1f)
         }
+        // Dilate the width and the height of the button
         growWidthAnimator!!.addUpdateListener {
             width = (it.animatedValue as Float)
             x = ((originalParams!!.x + (originalParams!!.width * 0.5) - (width * 0.5)).toFloat())
@@ -226,6 +246,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             y = (originalParams!!.y + (originalParams!!.height * 0.5) - (height * 0.5)).toFloat()
             button!!.layoutParams = LayoutParams(width.toInt(), height.toInt(), x.toInt(), y.toInt())
         }
+        // Set the properties for animation
         growAnimatorSet = AnimatorSet()
         growAnimatorSet!!.interpolator = EasingInterpolator(Ease.QUAD_IN_OUT)
         growAnimatorSet!!.play(growHeightAnimator!!).with(growWidthAnimator!!)
@@ -235,6 +256,9 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         growAnimatorSet!!.start()
     }
 
+    /*
+        Button transitions to the selected position and size
+     */
     private var translateAnimatorSet:AnimatorSet? = null
     private var translateWidthAnimator:ValueAnimator? = null
     private var selectAnimator:ValueAnimator? = null
@@ -242,6 +266,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
     private var targetWidth:Float = -1f
     private var targetX:Float = -1f
     fun select(targetX:Float, targetWidth:Float) {
+        // Cancel the select animation if its already running
         if (selectAnimator != null) {
             if (isSelectRunning) {
                 selectAnimator!!.cancel()
@@ -249,6 +274,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 selectAnimator = null
             }
         }
+        // Update the size of the button
         this.targetWidth = targetWidth
         this.targetX = targetX
         selectAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).
@@ -260,7 +286,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 button!!.layoutParams = LayoutParams(width.toInt(), height.toInt(), x.toInt(),
                     y.toInt())
         }
-
+        // Translate the position of the button
         if (targetWidth != -1f && targetX != -1f) {
             translateXAnimator = ValueAnimator.ofFloat((button!!.layoutParams as
                 LayoutParams).x.toFloat(), targetX)
@@ -272,7 +298,6 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             translateWidthAnimator = ValueAnimator.ofFloat((button!!.layoutParams as
                     LayoutParams).width.toFloat(), originalParams!!.width.toFloat())
         }
-
         translateXAnimator!!.addUpdateListener {
             x = (it.animatedValue as Float)
             button!!.layoutParams = LayoutParams(width.toInt(), height.toInt(), x.toInt(), y.toInt())
@@ -281,7 +306,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             width = (it.animatedValue as Float)
             button!!.layoutParams = LayoutParams(width.toInt(), height.toInt(), x.toInt(), y.toInt())
         }
-
+        // Create the properties for the animation
         translateAnimatorSet = AnimatorSet()
         translateAnimatorSet!!.play(selectAnimator!!).with(translateWidthAnimator!!).
             with(translateXAnimator!!)
@@ -292,10 +317,14 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         translateAnimatorSet!!.start()
     }
 
+    /*
+        Translate and transform the button to be unselected
+     */
     private var unSelectAnimatorSet:AnimatorSet? = null
     private var unSelectAnimator:ValueAnimator? = null
     private var isUnSelectRunning:Boolean = false
     fun unSelect() {
+        // If the unselected animator is running, cancel it
         if (unSelectAnimator != null) {
             if (isUnSelectRunning) {
                 unSelectAnimator!!.cancel()
@@ -303,6 +332,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 unSelectAnimator = null
             }
         }
+        // Dilate the height and the y position
         unSelectAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).
         height.toFloat(), minHeight.toFloat())
         unSelectAnimator!!.addUpdateListener {
@@ -312,6 +342,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             button!!.layoutParams = LayoutParams(width.toInt(), height.toInt(),
                 x.toInt(), y.toInt())
         }
+        // Translate the x position and dilate the width
         translateXAnimator = ValueAnimator.ofFloat((button!!.layoutParams as
                 LayoutParams).x.toFloat(), originalParams!!.x.toFloat())
         translateWidthAnimator = ValueAnimator.ofFloat((button!!.layoutParams as
@@ -324,6 +355,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             width = (it.animatedValue as Float)
             button!!.layoutParams = LayoutParams(width.toInt(), height.toInt(), x.toInt(), y.toInt())
         }
+        // Setup the properties for the animation set
         unSelectAnimatorSet = AnimatorSet()
         unSelectAnimatorSet!!.play(unSelectAnimator!!).with(translateXAnimator!!).
         with(translateWidthAnimator!!)
@@ -332,13 +364,15 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         unSelectAnimatorSet!!.duration = 500
         isUnSelectRunning = true
         unSelectAnimatorSet!!.start()
-
     }
 
     fun fadeIn() {
         fade(true, false, 1f, 0.125f)
     }
 
+    /*
+        Dilate and translate the button to shrink the button
+     */
     private var x: Float = 0f
     private var duration: Float = 0f
     private var translateXAnimator: ValueAnimator? = null
@@ -347,6 +381,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
     private var shrinkAnimationSetIsRunning: Boolean = false
     fun shrink() {
         grow = true
+        // If the shrink animation is running, cancel it
         if (shrinkAnimationSet != null) {
             if (shrinkAnimationSetIsRunning) {
                 shrinkAnimationSet!!.cancel()
@@ -354,6 +389,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 shrinkAnimationSet = null
             }
         }
+        // Select from which position the button shrinks
         when (shrinkType) {
             ShrinkType.left -> {
                 x = ((button!!.layoutParams as LayoutParams).x).toFloat()
@@ -369,12 +405,14 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 duration = 750f
             }
         }
+        // Translate the button's x position
         translateXAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).x.toFloat(), x)
         translateXAnimator!!.addUpdateListener {
             x = (it.animatedValue as Float)
             button!!.layoutParams =  LayoutParams(width.toInt(), (button!!.layoutParams as LayoutParams).height,
                 x.toInt(), (button!!.layoutParams as LayoutParams).y)
         }
+        // Dilate the width of the button
         shrinkWidthAnimator = ValueAnimator.ofFloat((button!!.layoutParams as LayoutParams).width.toFloat(), 0f)
         shrinkWidthAnimator!!.addUpdateListener {
             width = (it.animatedValue as Float)
@@ -386,15 +424,19 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         shrinkAnimationSet!!.play(translateXAnimator!!).with(shrinkWidthAnimator)
         shrinkAnimationSet!!.duration = duration.toLong()
         shrinkAnimationSet!!.start()
-        // Do on end
+        // Remove the button from the screen after it has shrunk
         shrinkAnimationSet!!.doOnEnd {
             parentLayout!!.removeView(this.getThis())
         }
     }
 
+    /*
+        Make the button appear or disappear
+     */
     private var fadeAnimator: ViewPropertyAnimator? = null
     private var fadeAnimatorIsRunning:Boolean = false
     fun fade(In:Boolean, Out:Boolean, Duration:Float, Delay:Float) {
+        // If the fading animation is runnning, cancel it
         if (fadeAnimator != null) {
             if (fadeAnimatorIsRunning) {
                 fadeAnimator!!.cancel()
@@ -402,6 +444,7 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
                 fadeAnimator = null
             }
         }
+        // Fade the button in or out
         if (In) {
             fadeAnimator = button!!.animate().alpha(1.0f)
             fadeAnimator!!.interpolator = EasingInterpolator(Ease.QUAD_IN_OUT)
@@ -410,11 +453,13 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
             fadeAnimator = button!!.animate().alpha(0.0f)
             fadeAnimator!!.interpolator = EasingInterpolator(Ease.QUAD_IN_OUT)
         }
+        // Setup the fade animation properties
         fadeAnimator!!.startDelay = (1000.0f * Delay).toLong()
         fadeAnimator!!.duration = (1000.0f * Duration).toLong()
         fadeAnimator!!.withStartAction {
             fadeAnimatorIsRunning = true
         }
+        // Decide whether to fade in our out or just stop after fading in
         fadeAnimator!!.withEndAction {
             if (In and Out) {
                 this.fade(In = false, Out = true, Duration = Duration, Delay = 0.0f)
@@ -429,6 +474,10 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         }
     }
 
+    /*
+        Returns the current background color of the button
+        based on the theme
+     */
     private fun getBackgroundColor(): Int {
         if (backgroundColor != null) {
             return backgroundColor!!
@@ -465,6 +514,9 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         shrunkParams = LayoutParams(0, 0,originalParams!!.x / 2, originalParams!!.y / 2)
     }
 
+    /*
+        Updates the style to a light theme
+     */
     private fun lightDominant() {
         if (backgroundColor == null) {
             button!!.setBackgroundColor(Color.BLACK)
@@ -474,6 +526,9 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         button!!.setTextColor(Color.WHITE)
     }
 
+    /*
+        Updates the style to a dark theme
+     */
     private fun darkDominant() {
         if (backgroundColor == null) {
             button!!.setBackgroundColor(Color.WHITE)
@@ -483,12 +538,14 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         button!!.setTextColor(Color.BLACK)
     }
 
+    /*
+        Draws the image selected by the user based on the theme
+     */
     fun loadImages(lightImageR:Int, darkImageR:Int) {
         this.lightImageR = lightImageR
         this.darkImageR = darkImageR
         fun lightDominant() {
             button!!.setBackgroundResource(lightImageR)
-
         }
         fun darkDominant() {
             button!!.setBackgroundResource(darkImageR)
@@ -500,6 +557,9 @@ class CButton(button: Button, parentLayout: AbsoluteLayout, params: LayoutParams
         }
     }
 
+    /*
+        Sets the appearance of the button based on the theme
+     */
     fun setStyle() {
         if (MainActivity.isThemeDark) {
             if (isInverted) {
